748b9a6be8e2154901cdbbaac0714946ae0632ff
emines0 <emin.sefa1@gmail.com>
2023-02-05 11:41:53 +0100

    display data of single person based on index, change index by clicking on the arrows

---
+++ README.md
@@ -0,0 +1,6 @@
+# List of Reviews
+
+- display data form data.jss
+- useState
+- display data of single person based on index
+- change index by clicking on the arrows
--- src/Review.js
+++ src/Review.js
@@ -1,9 +1,51 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from 'react'
+import people from './data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
 
 const Review = () => {
-  return <h2>review component</h2>;
-};
+  const [index, setIndex] = useState(0)
+  const { name, job, image, text } = people[index]
+  /*
+   * props of te people object based on index
+   * while index will be changed, the review will change too
+   */
 
-export default Review;
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1
+      return newIndex
+    })
+  }
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1
+      return newIndex
+    })
+  }
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn'>suprise me</button>
+    </article>
+  )
+}
+
+export default Review
